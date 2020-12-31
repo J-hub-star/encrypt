862f181788b1b370aedf14b034b85f472f18a912
J-hub-star <jaibehl33@gmail.com>
2020-12-31 16:06:21 -0600

    new

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/?Xor??", methods=['GET', 'POST'])
 def xor():
