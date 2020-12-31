6e3ad7dc6b68b87bfcb0fbdda0b300e05bcebfaf
J-hub-star <jaibehl33@gmail.com>
2020-12-31 16:03:17 -0600

    new

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from encrpytion import cipherText, generateKey, xoren,encrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
 
